a3fd54cdde0b9a90bf5619bc1feb0bbf903959e3
Ye Xu <xuyebear@gmail.com>
2021-09-05 00:09:05 -0500

    Updated code

--- mapping1_Lab1_part2_Ye.py
+++ mapping1_Lab1_part2_Ye.py
@@ -1,63 +1,47 @@
 import picar_4wd as fc
 import numpy as np
 import math
+import time
 import matplotlib
 import matplotlib.pyplot as plt
 
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 
-angleList = np.arange(-60,60,10)
-routeMap = np.zeros((200,100))
-carPos = [100, 0]
+angles = np.arange(-60,60,2)
+routeMap = np.zeros((200,200))
+carPos = [50, 0]
 
-matplotlib.use('Agg')
-
-angleList = np.arange(-60,60,10)
-routeMap = np.zeros((200,100))
-carPos = [100, 0]
-
-def get_coord_at(angle):
-    dist = fc.get_distance_at(angle)
-    if dist == -2:
-        x = -1
-        y = -1
-    else:
-        x = int(dist * np.sin(math.radians(angle))) + carPos[0]
-        y = int(dist * np.cos(math.radians(angle))) + carPos[1]
-   # print(dist)
-    return x, y
-
-def mapping(angleList):
+def get_coord_at(angles, carPos):
     x_coords = []
     y_coords = []
-    for angle in angleList:
-        x, y = get_coord_at(angle)
-        if x == -1 and y == -1:
+    for a in angles:
+        dist = fc.get_distance_at(a)
+        if dist == -2:
             continue
         else:
-            x_coords.append(x)
-            y_coords.append(y)
-   # x_coords = np.array(x_coords)
-   # y_coords = np.array(y_coords)
-   # print(x_coords)
-   # print(y_coords)
+            x = int(dist * np.sin(math.radians(a))) + carPos[0]
+            y = int(dist * np.cos(math.radians(a))) + carPos[1]
+        x_coords.append(x)
+        y_coords.append(y)
+        time.sleep(0.01)
+   # print(dist)
+    return x_coords, y_coords
 
-
-    for i in x_coords:
-        if i<0 or i>200:
-            continue
-        for j in y_coords:
-            if j > 100:
-                continue
-            routeMap[i,j] = 1
+def mapping(angles, carPos):
+    x_coords, y_coords = get_coord_at(angles, carPos)
+    for x,y in zip(x_coords, y_coords):
+            routeMap[x,y] = 1
+    routeMap[carPos[0], carPos[1]] = 1
     print(routeMap)
+    np.save('routeMap.npy', routeMap)
+
+
+    plt.matshow(routeMap)
+	plt.show()
+   #mat.pyplot.savefig("routemap.png")
     return routeMap
 
-def plot_map(routeMap):
-    plt.matshow(routeMap)
-    plt.show()
-
-
 if __name__ == "__main__":
-    mapping(angleList)
+    mapping(angles, carPos)
+
 
