d7fffdad0d65172524b06fabe549aad0a95e4121
Ye Xu <xuyebear@gmail.com>
2021-10-06 21:45:21 -0500

    Add files via upload
    script on server (Raspberry Pi) end

--- iot-labs/iot-lab-2/bluetooth/bt_server_ye.py
+++ iot-labs/iot-lab-2/bluetooth/bt_server_ye.py
@@ -1,6 +1,9 @@
 import bluetooth
 import picar_4wd as fc
 import json
+import time
+
+SPEED = 25
 
 hostMACAddress = "E4:5F:01:40:57:A3" # The address of Raspberry PI Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
 port = 0
@@ -12,32 +15,64 @@ s.listen(backlog)
 print("listening on port ", port)
 try:
     client, clientInfo = s.accept()
-    while 1:   
+    while 1:
         print("server recv from: ", clientInfo)
         data = client.recv(size)
         print('Receive direction from the client: ', data)
         if data != b"":
-            direction = data.decode()
-            results = fc.pi_read()
-            print('Pi car parameters before executing the direction from the client: ', results)
-            
-            if direction == 'left' :
-                results['Status'] = 'Turn left and move forward'
-            if direction == 'right' :
-                results['Status'] = 'Turn right and move forward'
-            if direction == 'forward' :
-                results['Status'] = 'Move forward'
-            if direction == 'backward':
-                results['Status'] = 'Move backward'
-            print('Pi car parameters after executing the direction from the client: ', results)
-            
-            result = json.dumps(results)
-            client.sendall(result.encode()) # Echo back to client
-                    
+            direction = data.decode().lower()
+            print('The direction Pi car received from the client: ', direction)
+
+            if direction == "forward":
+                fc.forward(SPEED)
+                distance = 0
+                for i in range(5):
+                    time.sleep(0.1)
+                    distance += SPEED * 0.1
+            if direction == "backward":
+                fc.backward(SPEED)
+                distance = 0
+                for i in range(5):
+                    time.sleep(0.1)
+                    distance += SPEED * 0.1
+
+            if direction == "left":
+                fc.turn_left(50)
+                for i in range(10):
+                    time.sleep(0.1)
+                fc.forward(SPEED)
+                distance = 0
+                for i in range(5):
+                    time.sleep(0.1)
+                    distance += SPEED * 0.1
+
+            if direction == "right":
+                fc.turn_right(50)
+                for i in range(10):
+                    time.sleep(0.1)
+
+                fc.forward(SPEED)
+                distance = 0
+                for i in range(5):
+                    time.sleep(0.1)
+                    distance += SPEED * 0.1
+
+            fc.stop()
+
+            # Read stats of pi car and add the distance and speed to the parameter list
+            stats = fc.pi_read()
+            stats["Direction"] = direction
+            stats["Travel_Distance"] = distance
+            stats["Car_Speed"] = SPEED
+            print('Pi car stats after executing the direction from the client: ', stats)
+
+            stats = json.dumps(stats)
+            client.sendall(stats.encode()) # Echo back to client
+
 #if data:
         #    print(data)
         #    client.send(data) # Echo back to client
-except: 
+except:
     print("Closing socket")
     client.close()
     s.close()
